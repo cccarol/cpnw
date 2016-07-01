2f8d95eb652eb857592e6dc56c62e725b9f25f00
Carol Lemke <carol.e.lemke@gmail.com>
2016-07-01 08:21:14 -0600

    Foundation v5.5.3 breaks w. newest version of jQuery. Switched to v2.2.4

--- partials/footer-scripts.php
+++ partials/footer-scripts.php
@@ -1,6 +1,6 @@
 <?php ?>
-
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<!-- jQuery v3.0.0 errors w. Foundation v5.5.3  USE 2.2.4 -->
+<script src="bower_components/jquery/dist/jquery.min.js"></script> 
 <script src="bower_components/foundation/js/foundation.min.js"></script>
 <script type="text/javascript" src="bower_components/superslides/dist/jquery.superslides.min.js"></script>
 <script type="text/javascript" src="bower_components/jquery-placeholder/jquery.placeholder.js"></script>
